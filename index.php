b73cc70326f2860c165c472d03f8c7ef42d84533
Radiant81 <91033429+Radiant81@users.noreply.github.com>
2021-09-19 22:20:26 +0000

    updates

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 	<head>
 		<title>Raphaël Pech - Portfolio</title>
 		<link rel="stylesheet" href="styles\bootstrap-3.3.7-dist\css\bootstrap.min.css">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 	</head>
+
+	<header>
+		<?php include_once("navbar.html"); ?>
+
+	</header>
 	<body>
 		<div class="container">
 			<h1>Hello World !</h1>
