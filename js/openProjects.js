e405f319e38ab25360dc3d8c65c9c1c3c2bc6bfe
Raph <91033429+raphaelpech@users.noreply.github.com>
2022-01-11 17:47:37 +0100

    Mise à jour page Réalisations

---
+++ js/openProjects.js
@@ -0,0 +1,48 @@
+/**
+ * Projet HTML/PHP/JS Caf'Art
+ */
+function openCafart()
+{
+    window.location.href = "realisations/cafart.html";
+}
+
+/**
+ * Projet Prestashop Purazuma
+ */
+function openPurazuma()
+{
+    window.location.href = "realisations/purazuma.html";
+}
+
+/**
+ * Projet C# 1: Gestion patients
+ */
+function openGPatients()
+{
+    window.location.href = "realisations/gestion-patients.html";
+}
+
+/**
+ * Projet C# 2: GSB GMIM
+ */
+function openGMIM()
+{
+    window.location.href = "realisations/gsb-gmim.html";
+}
+
+/**
+ * Projet Android GSB
+ */
+function openAndroid()
+{
+    window.location.href = "realisations/gsb-android.html";
+}
+
+/**
+ * Projet PHP
+ */
+function openPHP()
+{
+    alert("A venir!");
+}
+
